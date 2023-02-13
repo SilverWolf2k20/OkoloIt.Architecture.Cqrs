8c4ec23f76b8cdb3cbcb1ead9260cd94225f8df9
egorw <egorw@DESKTOP-TCDMSCK>
2023-02-14 01:21:31 +0300

    feat: добавлен файл сборки пакета

---
+++ build/build_package.ps1
@@ -0,0 +1,9 @@
+﻿# Taken from psake https://github.com/psake/psake
+
+# Clear Release folder
+Remove-Item -Path ..\src\bin\Release\*.nupkg
+dotnet clean ..\src\OkoloIt.Architecture.Cqrs.csproj -c Release;
+
+# Build Project
+dotnet build ..\src\OkoloIt.Architecture.Cqrs.csproj -c Release;
+dotnet pack ..\src\OkoloIt.Architecture.Cqrs.csproj -c Release;
